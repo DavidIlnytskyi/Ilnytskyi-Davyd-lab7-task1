18d8eabe33f5fb47af158e3c33c46cb423356fa8
David <ilnytskyi.pn@ucu.edu.ua>
2022-11-11 15:33:21 +0200

    added get_graph_from_file

---
+++ get_graph_from_file_function.py
@@ -0,0 +1,15 @@
+def get_graph_from_file(file: str) -> list:
+    """This function reads graphs from file and returns a list of graphs"""
+    graphs = []
+    list_graphs = []
+    file_content = open(file)
+    graphs = [list(row.strip().split(",")) for row in file_content]
+    i = 0
+    for element in graphs: 
+        list_graphs.append([])
+        for number in element: 
+            list_graphs[i].append(int(number))
+        i+=1
+    return list_graphs
+
+print(get_graph_from_file("test.txt"))
