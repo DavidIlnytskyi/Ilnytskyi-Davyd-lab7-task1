8b5daa981867e63230df5cd1c197c08ae13a3f10
David <ilnytskyi.pn@ucu.edu.ua>
2022-11-12 02:49:40 +0200

    added convert to dot function

---
+++ convert_to_dot.py
@@ -0,0 +1,18 @@
+def convert_to_dot(graph):
+    """This function writes a graph into a .dot file
+    >>> convert_to_dot(123)
+
+    """
+    if isinstance(graph, dict) is False: 
+        return None
+    connections = []
+    for element in graph.items():
+        for value in element[1]:
+            connections.append(f"{element[0]} -- {value}")
+    dot_file = open ("graph.dot", "w", encoding = "utf-8")
+    for connect in connections:
+        dot_file.write(connect + "\n")
+    dot_file.close()
+    
+import doctest 
+doctest.testmod()
